cbdb8960ff0fc1b721fa5ec77708256578298d7d
Sanuda24 <151610244+Sanuda24@users.noreply.github.com>
2024-08-10 11:03:52 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './components/Login';
-import Signup from './components/SignUp';
-import Home from './components/Home';
+import './App.css';
+import OrderManagement from './order';
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/Login" element={<Login />} />
-          <Route path="/SignUp" element={<Signup />} />
-          <Route path="/Home" element={<Home />} />
-          <Route path="/" element={<Login />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Pharmacy Order Management</h1>
+            </header>
+            <main>
+                <OrderManagement />
+            </main>
+        </div>
+    );
 }
 
 export default App;
