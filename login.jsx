4932e7fe7145a3e44aeb1ba0c7e828253b602c23
SewminPerera <151610723+SewminPerera@users.noreply.github.com>
2024-08-15 00:29:37 +0530

    Create login.jsx

---
+++ login.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import './LoginForm.css';
+import { FaUser } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+
+const LoginForm = () => {
+
+    
+
+  return (
+    
+    <div className='wrapper'> 
+        
+        <form action="">
+            <h1>Login</h1>  
+                <div className="input-box">
+                    <input type ="text" placeholder='Username' required />
+                    <FaUser className='icon' />
+                </div>
+                <div className="input-box">
+                    <input type="password" placeholder='Password' required />
+                    <FaLock className='icon'/>  
+                </div>
+
+                <div className="remember-forgot">
+                    <label><input type="checkbox"/>Remeber me </label>
+                    <a href='#'>Forgot password?</a>
+                </div>
+                <button type="submit">Login</button>
+
+                <div className="register-link">
+                    <p>Don't have an account? <a href="#">Register  </a></p>
+
+                </div>
+        </form>  
+
+       
+    </div>
+  )
+  
+}
+
+export default LoginForm
