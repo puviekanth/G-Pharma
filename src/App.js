aceb69f5faacf7e5e4ce2b84b13baa3dde37fd68
Puviekanth Balraj <puvie9edx@gmail.com>
2024-08-21 19:15:47 +0530

    Done all from my part

--- src/App.js
+++ src/App.js
@@ -20,35 +20,36 @@ import Payment from './components/Payment'
 import Individual from './components/individual'
 import Prescription from './components/Prescription'
 
+
 function App() {
   return (
     <Router>
-      <Routes>
-        {/* Redirect from the root path to /Home */}
-        <Route path="/" element={<Navigate to="/Home" />} />
-        
-        <Route path="/Home" element={<Home />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/SignUp" element={<Signup />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/Contact" element={<Contact />} />
-        <Route path="/Profile" element={<Profile />} />
-        <Route path="/Shop" element={<Shop />} />
-        <Route path="/Shop/ayurvedic" element={<Ayurvedic />} />
-        <Route path="/Shop/beauty" element={<Beauty />} />
-        <Route path="/Shop/baby" element={<Beauty />} />
-        <Route path="/Shop/multivitamins" element={<Baby />} />
-        <Route path="/Shop/machines" element={<Machine />} />
-        <Route path="/Shop/instruments" element={<Instrument />} />
-        <Route path="/Shop/vetenary" element={<Vetneray />} />
-        <Route path="/Shop/skincare" element={<Skin />} />
-        <Route path="/Shop/sexual-wellness" element={<Sexual />} />
-        <Route path="/Cart" element={<Cart />} />
-        <Route path="/Payment" element={<Payment />} />
-        <Route path="/Individual" element={<Individual />} />
-        <Route path='/Add-prescription' element={<Prescription />} />
-      </Routes>
-    </Router>
+    <Routes>
+      {/* Redirect from the root path to /Home */}
+      <Route path="/" element={<Navigate to="/Home" />} />
+      
+      <Route path="/Home" element={<Home />} />
+      <Route path="/Login" element={<Login />} />
+      <Route path="/SignUp" element={<Signup />} />
+      <Route path="/About" element={<About />} />
+      <Route path="/Contact" element={<Contact />} />
+      <Route path="/Profile" element={<Profile />} />
+      <Route path="/Shop" element={<Shop />} />
+      <Route path="/Shop/ayurvedic" element={<Ayurvedic />} />
+      <Route path="/Shop/beauty" element={<Beauty />} />
+      <Route path="/Shop/baby" element={<Beauty />} />
+      <Route path="/Shop/multivitamins" element={<Baby />} />
+      <Route path="/Shop/machines" element={<Machine />} />
+      <Route path="/Shop/instruments" element={<Instrument />} />
+      <Route path="/Shop/vetenary" element={<Vetneray />} />
+      <Route path="/Shop/skincare" element={<Skin />} />
+      <Route path="/Shop/sexual-wellness" element={<Sexual />} />
+      <Route path="/Cart" element={<Cart />} />
+      <Route path="/Payment" element={<Payment />} />
+      <Route path="/Individual" element={<Individual />} />
+      <Route path='/Add-prescription' element={<Prescription />} />
+    </Routes>
+  </Router>
   );
 }
 
