be84c7077f33857399067c9b16f092697d4614c1
Puviekanth Balraj <puvie9edx@gmail.com>
2024-10-05 23:58:28 +0530

    did image retrieval for products

--- server/index.js
+++ server/index.js
@@ -459,14 +459,22 @@ app.delete('/deleteOrder/:orderID',async (req,res)=>{
  
 
 //get porducts at admin page
-// app.get('/getproducts', async (req, res) => {
-//     try {
-//       const products = await Product.find();
-//       res.json(products);
-//     } catch (error) {
-//       res.status(500).json({ message: 'Error fetching products' });
-//     }
-//   });
+app.get('/getproducts', async (req, res) => {
+    try {
+      const products = await ProductModel.find();
+      console.log(products);
+      const productsData = products.map(product =>({
+        image:product.image,
+        name:product.name,
+        description: product.description,
+        price:product.price,
+        category:product.category
+      }))
+      res.json(productsData);
+    } catch (error) {
+      res.status(500).json({ message: 'Error fetching products',error });
+    }
+  });
 
 
  
