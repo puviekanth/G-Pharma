4e87c85164f10d3061423cceac75a527aec0678a
Puviekanth Balraj <puvie9edx@gmail.com>
2024-10-12 00:40:56 +0530

    started the hamburger menu

--- src/components/deliveryUI.js
+++ src/components/deliveryUI.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from 'react';
+import './delivery.css';
+import Logo from './images/Blue_and_White_Flat_Illustrative_Health_Products_Logo-removebg-preview.png'
 
-function DeliveryUI(){
-    return(
-        <>
-        <p>HEY</p>
-        </>
-    )
+function DeliveryUI() {
+  
+
+  
+
+  return (
+    <>
+    <div className='nav'>
+      <div className='header'>
+        <a href='/deliveryUI'><img src={Logo} className='logo' alt='logo' /></a>
+        <h4 className='brand'>Genuine Pharmacy</h4>
+      </div>
+     <div className='hamburger'>
+     <div className='bar' ></div>
+      <div className='bar' ></div>
+      <div className='bar' ></div>
+     </div>
+    </div>
+    </>
+  );
 }
 
 export default DeliveryUI;
